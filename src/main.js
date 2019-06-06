2629bdb390c04e0aa72941c379c275075678070b
郝洋洋 <yangyang_hao@fpi-inc.com>
2019-06-06 15:51:14 +0800

    添加配置文件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
 
-Vue.config.productionTip = false
+import App from '@/App.vue';
+import router from '@/router.js';
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+
+Vue.use(iView);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    router: router,
+    render: h => h(App)
+});
---
+++ src/router.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import helloWorld from '@/components/HelloWorld.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'helloWorld',
+      component: helloWorld,
+    },
+  ],
+});
